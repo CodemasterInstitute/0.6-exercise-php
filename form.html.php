048cc6baff4752ebd195a65be5947f24e320e821
Elizabeth H Burgess <me@graphitedge.com.au>
2016-05-10 14:07:14 +0800

    form solution

--- form.html.php
+++ form.html.php
@@ -6,8 +6,16 @@
 </head>
 <body>
 	<!-- Create your form here -->
+	<form method="post" action="add.php">
+
 	<!-- three inputs (name, balance, email)-->
+	<label for="name">Name: <input type="text" id="name" /></label>
+	<label for="email">Email address:<input type="email" id="email" /></label>
+	<label for="balance">Balance: <input type="text" id="balance"/> </label>
 	<!-- submit button -->
+	<input type="submit" value="submit"/>
+	</form>
 	<?php echo "test!"; ?>
+
 </body>
 </html>
